2ca176704eac724186595c6fbd372b65cc1056d2
mrtuck1 <65874314+mrtuck1@users.noreply.github.com>
2020-07-24 09:02:53 -0400

    today

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-$('input').on('change', function () {
-    var v = $(this).val();
-    $('div').css('font-size', v + 'px')
-    $('span').html(v);
-});
+var ChordSheetJS = require('chordsheetjs').default;
+
+
+import ChordSheetJS from 'chordsheetjs';
+
+const chords = document.getElementById('Chord-finder');
+const chordSheet = chords.substring(1);
+
+const parser = new ChordSheetJS.ChordSheetParser();
+const song = parser.parse(chordSheet);
+
+const formatter = new ChordSheetJS.TextFormatter();
+const disp = formatter.format(song);
+
+const formatter = new ChordSheetJS.HtmlTableFormatter();
+const disp = formatter.format(song);
+
+const formatter = new ChordSheetJS.HtmlDivFormatter();
+const disp = formatter.format(song);
+
+
+const displayChords = document.querySelectorAll('.display-chords');
